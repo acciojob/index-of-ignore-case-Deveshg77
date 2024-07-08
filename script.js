f7244db0eb667295bcf49b24816b96955f271bac
Devesh Sudhir Gunde <deveshgunde77@gmail.com>
2024-07-08 15:55:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	  var lowerS1 = s1.toLowerCase();
+  var lowerS2 = s2.toLowerCase();
+
+  // Find the index of the first occurrence of lowerS2 in lowerS1
+  var index = lowerS1.indexOf(lowerS2);
+
+  // Return the index
+  return index;
 }
 
 // Please do not change the code below
